4567bb6cf9d7da0650f32594d25fb279eab7ac16
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-02-13 13:13:59 +0200

    Add new handler to serve images

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 
 func main()  {
 	mux := http.NewServeMux()
+	imageMux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	imageMux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
 	server := http.Server{
 		Handler: mux,
 		Addr: ":8080",
