23cbdd989352464ad87d9331a14fffc1abe34ca1
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-03-09 15:03:06 +0200

    Create function to makeJwt

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -3,7 +3,10 @@ package auth
 import (
 	"errors"
 	"log"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -24,5 +27,23 @@ func CheckPassword (password string, hash string) error {
 	}
 
 	return nil
+
+}
+
+func MakeJWT(userId uuid.UUID, tokenSecret string, expiresIn time.Duration)(string, error){
+	claims := jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		IssuedAt: jwt.NewNumericDate(time.Now().UTC()) ,
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject: userId.String(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodES256,claims)
+	signedJWT, err :=token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		log.Printf("An error occured while signing your JWT: %s", err)
+		return "" , err
+	}
+
+	return signedJWT , nil
 
 }
