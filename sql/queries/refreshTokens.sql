82037dfddeee62fd6a7c16adb9afc338c894ec26
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-03-10 21:05:04 +0200

    Add query to revoke access tokens

--- sql/queries/refreshTokens.sql
+++ sql/queries/refreshTokens.sql
@@ -1,5 +1,28 @@
 -- name: CreateRefreshToken :exec
+INSERT INTO refresh_tokens (
+        token,
+        created_at,
+        updated_at,
+        user_id,
+        expires_at,
+        revoked_at
+    )
+VALUES (
+        $1,
+        NOW(),
+        NOW(),
+        $2,
+        NOW() + interval '60 days',
+        NULL
+    );
 
-INSERT INTO refresh_tokens (token, created_at, updated_at, user_id, expires_at, revoked_at)
-VALUES
-($1, NOW(), NOW(), $2, NOW() + interval '60 days', NULL);
+-- name: GetRefreshToken :one
+SELECT token, expires_at, user_id
+FROM refresh_tokens
+WHERE token = $1;
+
+
+-- name: RevokeRefreshToken :exec
+UPDATE refresh_tokens
+SET revoked_at = NOW()
+WHERE token = $1;
