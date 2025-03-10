32183e3ac7f35d24cf8a482e7464b939c1da9189
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-03-10 19:59:55 +0200

    Create query that creates refresh tokens in the database

---
+++ sql/queries/refreshTokens.sql
@@ -0,0 +1,5 @@
+-- name: CreateRefreshToken :exec
+
+INSERT INTO refresh_tokens (token, created_at, updated_at, user_id, expires_at, revoked_at)
+VALUES
+($1, NOW(), NOW(), $2, NOW() + interval '60 days', NULL);
