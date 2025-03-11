391b5aa2bbd145303b71712c1487f17e38defa55
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-03-11 06:08:07 +0200

    Add query to update email and password

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,13 +1,30 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email, hashed_password)
-VALUES (
-    gen_random_uuid(), NOW(), NOW(), $1, $2
-)
+INSERT INTO users (
+        id,
+        created_at,
+        updated_at,
+        email,
+        hashed_password
+    )
+VALUES (gen_random_uuid(), NOW(), NOW(), $1, $2)
 RETURNING *;
-
 -- name: DeleteUsers :exec
 DELETE FROM users;
-
+-- name: UpdatePasswordAndEmail :one
+UPDATE users
+SET email = $1,
+    hashed_password = $2,
+    updated_at = NOW()
+WHERE id = $3
+RETURNING id,
+    email,
+    updated_at,
+    created_at;
 -- name: GetUserByEmail :one
-SELECT  id, created_at, updated_at, email, hashed_password FROM users
+SELECT id,
+    created_at,
+    updated_at,
+    email,
+    hashed_password
+FROM users
 WHERE email = $1;
